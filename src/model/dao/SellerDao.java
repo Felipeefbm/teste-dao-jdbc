4e205ac65f5d5a7dbd497bc56b972cd0508e3f6f
Elvys Felipe <efbm009@gmail.com>
2023-01-13 16:38:35 -0300

    SellerDaojdbc and DaoFactory

--- src/model/dao/SellerDao.java
+++ src/model/dao/SellerDao.java
@@ -1,5 +1,20 @@
 package model.dao;
 
+
+import java.util.List;
+
+import model.entities.Seller;
+
+
 public interface SellerDao {
 
+	void insert (Seller obj);  // vai inserir este objeto no banco de dados
+	void update (Seller obj);
+	void DeleteById (Integer id);
+	Seller findById(Integer id); 
+	List<Seller> findall();
+
+	
+	
+	
 }
